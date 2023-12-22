0e4c4c46f573d533e196a85927c0ebf2bc584f37
miwer106 <michellewerth@hotmail.com>
2023-12-22 13:51:34 +0100

    UPDATE

--- docs/src/app/app.component.spec.ts
+++ docs/src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'michellewerth.github.io' title`, () => {
+  it(`should have the 'docs' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('michellewerth.github.io');
+    expect(app.title).toEqual('docs');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, michellewerth.github.io');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, docs');
   });
 });
